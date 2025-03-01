4237e797d3e8c362f0f65748fbd8678649262619
Jordan Higgins <jordanhiggins@usf.edu>
2025-03-01 12:19:09 -0500

    Task 1 - Created Book Class

---
+++ cc_11.js
@@ -0,0 +1,28 @@
+class Book {
+    constructor(title, author, isbn, copies) {
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
+      this.copies = copies;
+    }
+  
+    getDetails() {
+      return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.isbn}, Copies: ${this.copies}`;
+    }
+  
+    updateCopies(quantity) {
+      this.copies += quantity;
+      if (this.copies < 0) {
+        this.copies = 0;
+      }
+    }
+  } // Created the class book and added the constructor, getDetails and updateCopies methods, then prints everything to the console.
+  
+  // Test Cases
+  const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
+  console.log(book1.getDetails());
+  // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5"
+  
+  book1.updateCopies(-1);
+  console.log(book1.getDetails());
+  // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
